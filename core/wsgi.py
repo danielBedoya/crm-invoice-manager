d2a52a18f40d675eda5cd19049d0dc9f15d6c5ed
Daniel Bedoya <danielbedoyatbs@utp.edu.co>
2025-05-20 00:30:38 -0500

    Managing worker as a thread

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import threading
+from rq import Worker, Connection
+from django_rq import get_connection
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+def start_worker():
+    with Connection(get_connection("default")):
+        worker = Worker(["default"])
+        worker.work(burst=False)  # Set to False to keep it running continuously
+
+threading.Thread(target=start_worker, daemon=True).start()
